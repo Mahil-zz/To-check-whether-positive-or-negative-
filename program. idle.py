22135e467108177debeaa503476f9cb764824cbf
Mahil-zz <93509628+Mahil-zz@users.noreply.github.com>
2021-12-07 15:33:54 +0530

    Add files via upload

---
+++ program. idle.py
@@ -0,0 +1,11 @@
+print("To check positive or negative number")
+print("enter the number:")
+n=int(input())
+if(n>0):
+      print("the number is positive")
+elif(n==0):      
+      print("zero")
+else:
+      print("the number is negative")
+            
+     
